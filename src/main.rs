f6297863fec1895c0f795e63317dfc2b0b569702
Rostyslav Khudov <rhudov@gmail.com>
2024-12-01 13:40:10 +0000

    improvmenets to be prepared for the next days

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
-fn main() -> io::Result<()> {
-    let file = File::open("input.txt")?;
+fn read_file(filename: &str) -> Vec<String> {
+    let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
+    let mut content: Vec<String> = Vec::new();
+    for line in reader.lines() {
+        let line = line.unwrap();
+        content.push(line);
+    }
+    content
+}
+
+fn day_1(content: &Vec<String>) {
     let mut left_column: Vec<usize> = Vec::new();
     let mut right_column: Vec<usize> = Vec::new();
-    for line in reader.lines() {
-        let line = line?;
+    for line in content {
         let parts: Vec<&str> = line.split_whitespace().collect();
         left_column.push(parts[0].parse::<usize>().unwrap());
         right_column.push(parts[1].parse::<usize>().unwrap());
@@ -25,7 +33,13 @@ fn main() -> io::Result<()> {
         result_2 += element * count;
     }
 
+    println!("Day 1");
     println!("First part result: {:?}", result_1);
     println!("Second part result: {:?}", result_2);
+}
+
+fn main() -> io::Result<()> {
+    let content = read_file("input.txt");
+    day_1(&content);
     Ok(())
 }
